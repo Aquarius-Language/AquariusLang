d8ac7c1f89520d52b1fd19f84cb5e98a66cb1255
Temple Lin <78478355+TempleLin@users.noreply.github.com>
2023-01-19 05:05:53 +0800

    Lexer.

---
+++ AquariusLang/lexer/Lexer.cs
@@ -0,0 +1,170 @@
+﻿using AquariusLang.token;
+
+namespace AquariusLang.lexer; 
+
+public class Lexer {
+    private string input;     
+    private int position;     // current position in input (points to current char)
+    private int readPosition; // current reading position in input (after current char)
+    private char ch;          // current char under examination
+
+    /// <summary>
+    /// Singleton.
+    /// </summary>
+    /// <param name="input"></param>
+    public static Lexer NewInstance(string input) {
+        Lexer l = new Lexer() { input = input };
+        l.readChar();
+        return l;
+    }
+
+    public Token NextToken() {
+        Token token = new Token() {
+            Type = TokenType.ILLEGAL,
+            Literal = ""
+        };
+        
+        skipWhitespace();
+
+        switch (ch) {
+            case '=':
+                if (peekChar() == '=') {
+                    char _ch = ch;
+                    readChar();
+                    // string literal = char.ToString(_ch) + char.ToString(ch);
+                    string literal = new string(new[] { _ch, ch });
+                    token = newToken(TokenType.EQ, literal);
+                } else {
+                    token = newToken(TokenType.ASSIGN, ch);
+                }
+                break;
+            case '+':
+                token = newToken(TokenType.PLUS, ch);
+                break;
+            case '-':
+                token = newToken(TokenType.MINUS, ch);
+                break;
+            case '!':
+                // Two-character token: '!='.
+                if (peekChar() == '=') {
+                    char _ch = ch;
+                    readChar();
+                    string literal = new string(new[] { _ch, ch });
+                    token = newToken(TokenType.NOT_EQ, literal);
+                } else {
+                    token = newToken(TokenType.BANG, ch);
+                }
+                break;
+            case '/':
+                token = newToken(TokenType.SLASH, ch);
+                break;
+            case '*':
+                token = newToken(TokenType.ASTERISK, ch);
+                break;
+            case '<':
+                token = newToken(TokenType.LT, ch);
+                break;
+            case '>':
+                token = newToken(TokenType.GT, ch);
+                break;
+            case ';':
+                token = newToken(TokenType.SEMICOLON, ch);
+                break;
+            case ',':
+                token = newToken(TokenType.COMMA, ch);
+                break;
+            case '{' :
+                token = newToken(TokenType.LBRACE, ch);
+                break;
+            case '}':
+                token = newToken(TokenType.RBRACE, ch);
+                break;
+            case '(':
+                token = newToken(TokenType.LPAREN, ch);
+                break;
+            case ')':
+                token = newToken(TokenType.RPAREN, ch);
+                break;
+            case (char)0:
+                token = newToken(TokenType.EOF, "");
+                break;
+            default:
+                if (isLetter(ch)) { // Check if it is identifier token or a keyword token.
+                    string literal = readIdentifier();
+                    string type = TokenLookup.LookupIdentifier(literal);
+                    token = newToken(type, literal);
+                    return token;
+                } else if (isDigit(ch)) { // Check if is number.
+                    string literal = readNumber();
+                    token = newToken(TokenType.INT, literal);
+                    return token;
+                } else {
+                    token = newToken(TokenType.ILLEGAL, ch);
+                }
+                break;
+        }
+
+        readChar();
+        return token;
+    }
+
+    private void skipWhitespace() {
+        while (ch is ' ' or '\t' or '\n' or '\r') {
+            readChar();
+        }
+    }
+
+    private void readChar() {
+        if (readPosition >= input.Length) {
+            ch = (char)0;
+        } else {
+            ch = input[readPosition];
+        }
+
+        position = readPosition;
+        readPosition++;
+    }
+
+    /// <summary>
+    /// peekChar() is really similar to readChar(), except that it doesn’t increment l.position and l.readPosition.
+    /// </summary>
+    /// <returns></returns>
+    private char peekChar() {
+        return (readPosition >= input.Length) ? (char)0 : input[readPosition];
+    }
+
+    private string readIdentifier() {
+        int lastPos = position;
+        while (isLetter(ch)) {
+            readChar();
+        }
+
+        return input.Substring(lastPos, position - lastPos);
+    }
+
+    private string readNumber() {
+        int lastPos = position;
+        while (isDigit(ch)) {
+            readChar();
+        }
+
+        return input.Substring(lastPos, position - lastPos);
+    }
+
+    private bool isLetter(char ch) {
+        // '_' is also a valid letter. This makes it possible to use foo_bar as an identifier.
+        return ch is >= 'a' and <= 'z' or >= 'A' and <= 'Z' or '_';
+    }
+
+    private bool isDigit(char ch) {
+        return ch is >= '0' and <= '9';
+    }
+
+    private Token newToken(string tokenType, string literal) {
+        return new Token() { Type = tokenType, Literal = literal };
+    }
+    
+    private Token newToken(string tokenType, char ch) {
+        return new Token() { Type = tokenType, Literal = char.ToString(ch) };
+    }
+}
---
+++ AquariusLang/lexer/LexerTest.cs
@@ -0,0 +1,135 @@
+﻿using AquariusLang.token;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AquariusLang.lexer; 
+
+public class LexerTest {
+    /// <summary>
+    /// For logging outputs during testing.
+    /// </summary>
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public LexerTest(ITestOutputHelper testOutputHelper) {
+        _testOutputHelper = testOutputHelper;
+    }
+    
+    struct ExpectedTest {
+        public string ExpectedType { get; set; }
+        public string ExpectedLiteral { get; set; }
+    }
+    
+    [Fact]
+    public void TestNextToken() {
+        string input = @"
+            let five = 5;
+            let ten = 10;
+
+            let add = fn(x, y) {
+              x + y;
+            };
+
+            let result = add(five, ten);
+            !-/*5;
+            5 < 10 > 5;
+
+            if (5 < 10) {
+	            return true;
+            } else {
+	            return false;
+            }
+
+            10 == 10;
+            10 != 9;
+        ";
+        
+        ExpectedTest[] tests = new [] {
+            new ExpectedTest(){ExpectedType = TokenType.LET,       ExpectedLiteral = "let"},
+            new ExpectedTest(){ExpectedType = TokenType.IDENT,     ExpectedLiteral = "five"},
+            new ExpectedTest(){ExpectedType = TokenType.ASSIGN,    ExpectedLiteral = "="},
+            new ExpectedTest(){ExpectedType = TokenType.INT,       ExpectedLiteral = "5"},
+            new ExpectedTest(){ExpectedType = TokenType.SEMICOLON, ExpectedLiteral = ";"},
+            new ExpectedTest(){ExpectedType = TokenType.LET,       ExpectedLiteral = "let"},
+            new ExpectedTest(){ExpectedType = TokenType.IDENT,     ExpectedLiteral = "ten"},
+            new ExpectedTest(){ExpectedType = TokenType.ASSIGN,    ExpectedLiteral = "="},
+            new ExpectedTest(){ExpectedType = TokenType.INT,       ExpectedLiteral = "10"},
+            new ExpectedTest(){ExpectedType = TokenType.SEMICOLON, ExpectedLiteral = ";"},
+            new ExpectedTest(){ExpectedType = TokenType.LET,       ExpectedLiteral = "let"},
+            new ExpectedTest(){ExpectedType = TokenType.IDENT,     ExpectedLiteral = "add"},
+            new ExpectedTest(){ExpectedType = TokenType.ASSIGN,    ExpectedLiteral = "="},
+            new ExpectedTest(){ExpectedType = TokenType.FUNCTION,  ExpectedLiteral = "fn"},
+            new ExpectedTest(){ExpectedType = TokenType.LPAREN,    ExpectedLiteral = "("},
+            new ExpectedTest(){ExpectedType = TokenType.IDENT,     ExpectedLiteral = "x"},
+            new ExpectedTest(){ExpectedType = TokenType.COMMA,     ExpectedLiteral = ","},
+            new ExpectedTest(){ExpectedType = TokenType.IDENT,     ExpectedLiteral = "y"},
+            new ExpectedTest(){ExpectedType = TokenType.RPAREN,    ExpectedLiteral = ")"},
+            new ExpectedTest(){ExpectedType = TokenType.LBRACE,    ExpectedLiteral = "{"},
+            new ExpectedTest(){ExpectedType = TokenType.IDENT,     ExpectedLiteral = "x"},
+            new ExpectedTest(){ExpectedType = TokenType.PLUS,      ExpectedLiteral = "+"},
+            new ExpectedTest(){ExpectedType = TokenType.IDENT,     ExpectedLiteral = "y"},
+            new ExpectedTest(){ExpectedType = TokenType.SEMICOLON, ExpectedLiteral = ";"},
+            new ExpectedTest(){ExpectedType = TokenType.RBRACE,    ExpectedLiteral = "}"},
+            new ExpectedTest(){ExpectedType = TokenType.SEMICOLON, ExpectedLiteral = ";"},
+            new ExpectedTest(){ExpectedType = TokenType.LET,       ExpectedLiteral = "let"},
+            new ExpectedTest(){ExpectedType = TokenType.IDENT,     ExpectedLiteral = "result"},
+            new ExpectedTest(){ExpectedType = TokenType.ASSIGN,    ExpectedLiteral = "="},
+            new ExpectedTest(){ExpectedType = TokenType.IDENT,     ExpectedLiteral = "add"},
+            new ExpectedTest(){ExpectedType = TokenType.LPAREN,    ExpectedLiteral = "("},
+            new ExpectedTest(){ExpectedType = TokenType.IDENT,     ExpectedLiteral = "five"},
+            new ExpectedTest(){ExpectedType = TokenType.COMMA,     ExpectedLiteral = ","},
+            new ExpectedTest(){ExpectedType = TokenType.IDENT,     ExpectedLiteral = "ten"},
+            new ExpectedTest(){ExpectedType = TokenType.RPAREN,    ExpectedLiteral = ")"},
+            new ExpectedTest(){ExpectedType = TokenType.SEMICOLON, ExpectedLiteral = ";"},
+            new ExpectedTest(){ExpectedType = TokenType.BANG,      ExpectedLiteral = "!"},
+            new ExpectedTest(){ExpectedType = TokenType.MINUS,     ExpectedLiteral = "-"},
+            new ExpectedTest(){ExpectedType = TokenType.SLASH,     ExpectedLiteral = "/"},
+            new ExpectedTest(){ExpectedType = TokenType.ASTERISK,  ExpectedLiteral = "*"},
+            new ExpectedTest(){ExpectedType = TokenType.INT,       ExpectedLiteral = "5"},
+            new ExpectedTest(){ExpectedType = TokenType.SEMICOLON, ExpectedLiteral = ";"},
+            new ExpectedTest(){ExpectedType = TokenType.INT,       ExpectedLiteral = "5"},
+            new ExpectedTest(){ExpectedType = TokenType.LT,        ExpectedLiteral = "<"},
+            new ExpectedTest(){ExpectedType = TokenType.INT,       ExpectedLiteral = "10"},
+            new ExpectedTest(){ExpectedType = TokenType.GT,        ExpectedLiteral = ">"},
+            new ExpectedTest(){ExpectedType = TokenType.INT,       ExpectedLiteral = "5"},
+            new ExpectedTest(){ExpectedType = TokenType.SEMICOLON, ExpectedLiteral = ";"},
+            new ExpectedTest(){ExpectedType = TokenType.IF,        ExpectedLiteral = "if"},
+            new ExpectedTest(){ExpectedType = TokenType.LPAREN,    ExpectedLiteral = "("},
+            new ExpectedTest(){ExpectedType = TokenType.INT,       ExpectedLiteral = "5"},
+            new ExpectedTest(){ExpectedType = TokenType.LT,        ExpectedLiteral = "<"},
+            new ExpectedTest(){ExpectedType = TokenType.INT,       ExpectedLiteral = "10"},
+            new ExpectedTest(){ExpectedType = TokenType.RPAREN,    ExpectedLiteral = ")"},
+            new ExpectedTest(){ExpectedType = TokenType.LBRACE,    ExpectedLiteral = "{"},
+            new ExpectedTest(){ExpectedType = TokenType.RETURN,    ExpectedLiteral = "return"},
+            new ExpectedTest(){ExpectedType = TokenType.TRUE,      ExpectedLiteral = "true"},
+            new ExpectedTest(){ExpectedType = TokenType.SEMICOLON, ExpectedLiteral = ";"},
+            new ExpectedTest(){ExpectedType = TokenType.RBRACE,    ExpectedLiteral = "}"},
+            new ExpectedTest(){ExpectedType = TokenType.ELSE,      ExpectedLiteral = "else"},
+            new ExpectedTest(){ExpectedType = TokenType.LBRACE,    ExpectedLiteral = "{"},
+            new ExpectedTest(){ExpectedType = TokenType.RETURN,    ExpectedLiteral = "return"},
+            new ExpectedTest(){ExpectedType = TokenType.FALSE,     ExpectedLiteral = "false"},
+            new ExpectedTest(){ExpectedType = TokenType.SEMICOLON, ExpectedLiteral = ";"},
+            new ExpectedTest(){ExpectedType = TokenType.RBRACE,    ExpectedLiteral = "}"},
+            new ExpectedTest(){ExpectedType = TokenType.INT,       ExpectedLiteral = "10"},
+            new ExpectedTest(){ExpectedType = TokenType.EQ,        ExpectedLiteral = "=="},
+            new ExpectedTest(){ExpectedType = TokenType.INT,       ExpectedLiteral = "10"},
+            new ExpectedTest(){ExpectedType = TokenType.SEMICOLON, ExpectedLiteral = ";"},
+            new ExpectedTest(){ExpectedType = TokenType.INT,       ExpectedLiteral = "10"},
+            new ExpectedTest(){ExpectedType = TokenType.NOT_EQ,    ExpectedLiteral = "!="},
+            new ExpectedTest(){ExpectedType = TokenType.INT,       ExpectedLiteral = "9"},
+            new ExpectedTest(){ExpectedType = TokenType.SEMICOLON, ExpectedLiteral = ";"},
+            new ExpectedTest(){ExpectedType = TokenType.EOF,       ExpectedLiteral = ""},
+        };
+        
+        Lexer lexer = Lexer.NewInstance(input);
+
+        foreach (var expectedTest in tests) {
+            Token token = lexer.NextToken();
+            
+            _testOutputHelper.WriteLine("Token -    " + token);
+            _testOutputHelper.WriteLine("Expected - " + $"Type: {expectedTest.ExpectedType}, Literal: {expectedTest.ExpectedLiteral}");
+
+            Assert.Equal(token.Type, expectedTest.ExpectedType);
+            Assert.Equal(token.Literal, expectedTest.ExpectedLiteral);
+        }
+    }
+}
